10c52b4d03cd70d53571d44cb1d6141c6a22025d
DeekshithVanka <vankadeekshith333@gmail.com>
2025-03-30 12:43:04 +0530

    5th commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,27 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
+
+// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    emptyOutDir: true,
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: process.env.VITE_BACKEND_URL || 'http://localhost:3007',
-        changeOrigin: true,
-        secure: false,
-      },
-      '/socket.io': {
-        target: process.env.VITE_BACKEND_URL || 'http://localhost:3007',
-        changeOrigin: true,
-        secure: false,
-        ws: true,
-      },
-    },
-  },
-});
+   plugins:[react(),tailwindcss()], 
+})
